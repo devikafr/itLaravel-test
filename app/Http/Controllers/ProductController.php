a21cbbe364c325b77e8cf5922d7733311df56f5d
devikafr <iamdarshani@yahoo.co.uk>
2017-02-09 15:51:11 +0000

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+  public function index()
+  {
+    $products = Product::all();
+    return view('product', ['products' => $products]);
+
+  }
+}
